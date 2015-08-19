0316f9069c12daac52e8195a3ce6f8cd96b91469
Shing Lyu <shing.lyu@gmail.com>
2015-08-19 11:24:55 +0800

    Fixed orm parse step bug

--- orm.py
+++ orm.py
@@ -97,7 +97,7 @@ def parseCaseStep(case_step_txt):
     case_step = []
 
     if len(steps) == 0:
-        return parseStep(0, [case_step_txt, ''])
+        return [parseStep(0, [case_step_txt, ''])]
 
     for index, step in enumerate(steps):
         case_step.append(parseStep(index + 1, step))
